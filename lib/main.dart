a2d84cf5b00297bcf9950332d596caec5d2cd1e0
TousifMitul <tousif5robbani@gmail.com>
2026-04-20 19:03:05 +0600

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:expense_tracker/view/main_tab/main_tab_view.dart';
 import 'package:flutter/material.dart';
+
+import 'common/color_extension.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,7 +10,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
