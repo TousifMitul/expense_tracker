1f68b498ec3fa7e934208493b15d4ba5a087c589
TousifMitul <tousif5robbani@gmail.com>
2026-04-20 17:33:03 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Expense_Tracker',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: "Inter",
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: TColor.primary,
+          background: TColor.gray80,
+          primary: TColor.primary,
+          primaryContainer: TColor.gray60,
+          secondary: TColor.secondary,
+        ),
+        useMaterial3: false,
+      ),
+      home: const MainTabView(),
+    );
+  }
+}
